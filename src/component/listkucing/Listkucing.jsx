504a41d018f524d5cde0ed93b3b89441352de2fa
haris <harisfirman387@gmail.com>
2023-07-14 16:37:44 +0700

    change nodemodules to linux

--- src/component/listkucing/Listkucing.jsx
+++ src/component/listkucing/Listkucing.jsx
@@ -1,44 +1,7 @@
-import React from "react";
+import React from 'react'
 
-const KucingRas = () => {
-  const listKucing = [
-    {
-      ras: "Persia",
-      gambar: "/images/persia.jpg",
-      deskripsi: "Kucing dengan bulu panjang dan wajah pesek",
-    },
-    {
-      ras: "Maine Coon",
-      gambar: "/images/maine-coon.jpg",
-      deskripsi: "Kucing dengan ukuran tubuh besar dan bulu tebal",
-    },
-    // ... dan seterusnya
-  ];
-
-  const handleSearch = (keyword) => {
-    // Logika pencarian berdasarkan keyword (misalnya nama ras) di sini
-    // Anda dapat mengatur state atau melakukan operasi lain sesuai kebutuhan aplikasi
-  };
-
+export const Listkucing = () => {
   return (
-    <div>
-      <h2>Daftar Ras Kucing</h2>
-      <input
-        type="text"
-        onChange={(e) => handleSearch(e.target.value)}
-        placeholder="Cari ras kucing"
-      />
-      <ul>
-        {listKucing.map((kucing, index) => (
-          <li key={index}>
-            <img src={kucing.gambar} alt={kucing.ras} />
-            <h3>{kucing.ras}</h3>
-            <p>{kucing.deskripsi}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default KucingRas;
+    <div>Listkucing</div>
+  )
+}
